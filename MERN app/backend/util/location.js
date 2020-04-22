ec48e387e650f254aae373ede8028b32168b9aec
mariaruots <56537966+mariaruots@users.noreply.github.com>
2020-04-22 16:12:32 +0300

    whole project upload

---
+++ MERN app/backend/util/location.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+
+const HttpError = require("../models/http-error");
+
+const API_KEY = process.env.GOOGLE_API_KEY;
+
+async function getCoordsForAddress(address) {
+  // if no api key --> dummy code:
+  // return {
+  //    lat: 40.7484474,
+  //    lng: -73.9871516
+  //  };
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
+
+  const data = response.data;
+
+  //console.dir(data);
+
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for the specified address.",
+      422
+    );
+    throw error;
+  }
+
+  const coordinates = data.results[0].geometry.location;
+
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
